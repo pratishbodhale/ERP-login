106e1a5470d3f76111bbd1bababf1b38a8b07ced
Pratish Bodhale <pratish.bodhale2@gmail.com>
2017-12-20 12:45:32 +0530

    Add files via upload
    Initial commit

---
+++ exec.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	//********************** THIS PART SHOUlD BE EDITED ****************//
+	// Roll number goes here
+	var Roll_No = 'XXXXXXXXX';
+	// Pasword
+	var Password = '*****';
+
+	// Specify all the questions and answers as it is considering Uppercase and Lowercase
+	var qt1 = "Question 1 goes here";
+	var ans1 = "Answer for Question 1";
+
+	var qt2 = "Question 2 goes here";
+	var ans2 = "Answer for Question 2";
+
+	var qt3 = "Question 3 goes here";
+	var ans3 = "Answer for Question 3";
+
+	//******************************************************************//
+
+	$('#user_id').val(Roll_No);
+	$('#password').val(Password);
+	$('#user_id').select();
+	$('#password').select();
+
+	setTimeout(function() {
+	    if($('#question').text()==qt1){
+	    	$('#answer').val(ans1);
+	    }
+	    else if($('#question').text()==qt2){
+	    	$('#answer').val(ans2);
+	    }
+	    else if($('#question').text()==qt3){
+	    	$('#answer').val(ans3);
+	    }
+	    else{
+	    	alert('Please reload this page !!');
+	    	$('.panel-heading').text("Time out !!!!, Please Refresh the Page.");
+	    }
+	    $('input[type=submit]').click();
+ 	 }, 500);
+});
